e3b864e1bba1861a57fc34ad94b35bb0fc6436d5
Pantelis <simeonidispant@gamil.com>
2023-12-03 17:56:54 +0200

    update

--- data_manipulation.py
+++ data_manipulation.py
@@ -2,12 +2,13 @@ import pandas as pd
 # import sqlite3
 from glob import glob
 import mariadb 
+from sqlalchemy import create_engine
 
 # Function to calculate KPI1 and KPI2
 def calculate_kpis(input_files):
     # Initialize dataframes for KPIs
-    kpi1_df = pd.DataFrame(columns=['interval_start_timestamp', 'interval_end_timestamp', 'service_id', 'total_bytes', 'interval'])
-    kpi2_df = pd.DataFrame(columns=['interval_start_timestamp', 'interval_end_timestamp', 'cell_id', 'number_of_unique_users', 'interval'])
+    kpi1_df = pd.DataFrame(columns=['interval_start_timestamp', 'interval_end_timestamp', 'service_id', 'total_bytes', 'interval_period'])
+    kpi2_df = pd.DataFrame(columns=['interval_start_timestamp', 'interval_end_timestamp', 'cell_id', 'number_of_unique_users', 'interval_period'])
     df = []
     for file in input_files:
         # Read CSV file into a DataFrame
@@ -23,7 +24,7 @@ def calculate_kpis(input_files):
             'interval_end_timestamp': big_df['interval_end_timestamp'].max(),
             'service_id': kpi1.index[i],
             'total_bytes': kpi1.values[i],
-            'interval': '5-minute'
+            'interval_period': '5-minute'
         }, ignore_index=True)
     
     # Calculate KPI2: Top 3 cells by number of unique users
@@ -34,36 +35,30 @@ def calculate_kpis(input_files):
             'interval_end_timestamp': big_df['interval_end_timestamp'].max(),
             'cell_id': kpi2.index[i],
             'number_of_unique_users': kpi2.values[i],
-            'interval': '5-minute'
+            'interval_period': '5-minute'
         }, ignore_index=True)
 
     return kpi1_df, kpi2_df
 
 # Function to store KPIs in the database
-def store_kpis_in_database(kpi1_df, kpi2_df, database_name='mobile_kpis.db'):
-    conn = mariadb.connect(
-        user="db_user",
-        password="db_user_passwd",
-        host="localhost",
-        database= database_name)
-    with conn.cursor() as cursor:
-        try: 
-            # Store KPIs in MariaDB
-            kpi1_df.to_sql('kpi1_table', cursor, if_exists='append', index=False, method='multi', chunksize=1000)
-            kpi2_df.to_sql('kpi2_table', cursor, if_exists='append', index=False, method='multi', chunksize=1000)
+def store_kpis_in_database(kpi1_df, kpi2_df):
+    engine = create_engine("mariadb+pymysql://root:example@localhost/ETL?charset=utf8mb4")
+   
+    try: 
+        # Store KPIs in MariaDB
+        kpi1_df.to_sql('KP1', engine, if_exists='append', index=False, method='multi', chunksize=1000)
+        kpi2_df.to_sql('KP2', engine, if_exists='append', index=False, method='multi', chunksize=1000)
+        # Enhancment: check for uniqueness of values inserted
 
-        except mariadb.Error as e: 
-            print(f"Error: {e}")
-
-        cursor.commit() 
-        print(f"Last Inserted ID: {cursor.lastrowid}")
+    except mariadb.Error as e: 
+        print(f"Error: {e}")
         
    
 
 # Main function
 def main():
     # Specify the path to the raw files
-    raw_files_path = '/mnt/c/Users/simeo/Desktop/Commsquare_Python_Software_Engineer_Programming_Challenge/**.txt'
+    raw_files_path = '**.txt'
 
     # Get a list of all raw files
     raw_files = glob(raw_files_path)
